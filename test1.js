6f2c2442b492d9f3b0af26de8f5d0cafa004d2a5
fastarms <57482753+fastarms@users.noreply.github.com>
2020-04-25 08:25:14 +0400

    Add files via upload

---
+++ test1.js
@@ -0,0 +1,6 @@
+const link={'youtube':['https://www.youtube.com/watch?v=GFtOmig0TlQ',
+'https://www.youtube.com/watch?v=JWJ81mokqx0',
+'https://www.youtube.com/watch?v=EemQvElZGbU'],
+"pornactress":['monique alexander']}
+
+module.exports=link
---
+++ test2.js
@@ -0,0 +1,6 @@
+//const chalk = require('chalk');
+const test = require('./test1');
+
+  for(let item of test.youtube){
+    console.log(item)
+  }
