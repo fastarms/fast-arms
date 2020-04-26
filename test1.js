f93caf35a61da261fee727a4e1ef4c25212a518a
fastarms <57482753+fastarms@users.noreply.github.com>
2020-04-26 09:26:23 +0400

    Delete test1.js

--- test1.js
+++
@@ -1,6 +0,0 @@
-const link={'youtube':['https://www.youtube.com/watch?v=GFtOmig0TlQ',
-'https://www.youtube.com/watch?v=JWJ81mokqx0',
-'https://www.youtube.com/watch?v=EemQvElZGbU'],
-"pornactress":['monique alexander']}
-
-module.exports=link
